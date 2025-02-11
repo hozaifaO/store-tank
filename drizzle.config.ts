a7972390f3e2a88f37f5df78cc8d843f24e179ac
hozaifaO <hozaifaowaisi.h@gmail.com>
2025-02-11 16:21:25 -0500

    Fixed Version issue with drizzle and setup drizzle connection with db using pooling and tested with user schema migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,17 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
+  out: "./drizzle",
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["store-tank_*"],
+  dbCredentials: {
+    host: env.SINGLESTORE_HOST,
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    port: parseInt(env.SINGLESTORE_PORT),
+    ssl: {},
+
+  }
 } satisfies Config;
---
+++ drizzle/0000_sharp_ultron.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users_table` (
+	`id` int NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`age` int NOT NULL,
+	CONSTRAINT `users_table_id` PRIMARY KEY(`id`)
+);
